db376b5f2333dda53081b1a8b825638b4cf082f4
Iulia Groza <iuliag523@gmail.com>
2023-11-10 18:17:30 +0200

    Update First Labs

--- L2/symbol_table.rs
+++ L2/symbol_table.rs
@@ -1,71 +1,72 @@
+use std::fmt;
+
 const TABLE_SIZE: usize = 256; // A constant for the size of the table
 
 // A struct representing an entry in our hash table
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Entry {
     key: String,
     value: String,
 }
 
-struct HashTable {
+#[derive(Debug)]
+pub(crate) struct HashTable {
     table: Vec<Option<Vec<Entry>>>, // A vector of options containing vectors of entries
 }
 
 impl HashTable {
-    fn new() -> Self {
+    pub fn new() -> Self {
         HashTable {
             table: vec![None; TABLE_SIZE],
         }
     }
 
-    fn hash(&self, key: &str) -> usize {
+    pub fn hash(&self, key: &str) -> usize {
         key.bytes()
             .fold(0, |acc, b| (acc + b as usize) % TABLE_SIZE)
     }
 
-    // Insert a key-value pair into the hash table
-    fn insert(&mut self, key: String, value: String) {
+    pub fn insert(&mut self, key: String, value: String) -> usize {
         let idx = self.hash(&key);
+
+        // Initialize the position to the index of the bucket
+        let position = idx;
+
         match &mut self.table[idx] {
             Some(bucket) => {
-                for entry in bucket.iter_mut() {
+                for (i, entry) in bucket.iter_mut().enumerate() {
                     if entry.key == key {
                         entry.value = value; // Update the value if key already exists
-                        return;
+                        return position + i; // Return the unique position
                     }
                 }
                 bucket.push(Entry { key, value }); // Add a new entry if key not found
+                position + bucket.len() - 1 // Return the new position
             }
             None => {
-                let bucket = vec![Entry { key, value }];
-                self.table[idx] = Some(bucket);
+                self.table[idx] = Some(vec![Entry { key, value }]);
+                position // The position is the start of the new bucket
             }
-        }
-    }
-
-    // Get a value for a given key
-    fn get(&self, key: &str) -> Option<&String> {
-        let idx = self.hash(key);
-        match &self.table[idx] {
-            Some(bucket) => {
-                for entry in bucket.iter() {
-                    if entry.key == key {
-                        return Some(&entry.value);
-                    }
-                }
-                None
-            }
-            None => None,
         }
     }
 }
 
-// Test
-fn main() {
-    let mut ht = HashTable::new();
-    ht.insert("identifier".to_string(), "value".to_string());
-    ht.insert("constant".to_string(), "42".to_string());
+impl fmt::Display for HashTable {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Start with an empty string
+        let mut table_str = String::new();
 
-    println!("{:?}", ht.get("identifier"));
-    println!("{:?}", ht.get("constant"));
+        // Iterate over the hash table and append each key-value pair to the string
+        for (i, bucket) in self.table.iter().enumerate() {
+            if let Some(entries) = bucket {
+                for entry in entries {
+                    // Change the format to (hash_table_position, token)
+                    table_str.push_str(&format!("({}, {}), ", i, entry.key));
+                }
+            }
+        }
+
+        // Write the formatted string to the formatter
+        write!(f, "{}", table_str)
+    }
 }
